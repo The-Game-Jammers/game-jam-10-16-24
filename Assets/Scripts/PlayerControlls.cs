960606f919c4ff1a3d2f065bdb601d4aee31311b
superinjustice1000 <110139914+superinjustice1000@users.noreply.github.com>
2024-10-17 23:52:13 -0400

    Player Move and Camera 0.0.3
    The Player character now moves with WASD, also added a camera that moves when the character reaches the edge.

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerControlls : MonoBehaviour
 {
     Rigidbody2D rigidLink;
+    Vector2 moveInput;
     float moveInputX;
     float moveInputY;
     float velocityX;
     float velocityY;
     [SerializeField] float speedModifier = 100f;
+    [SerializeField] GameObject spawnLocation;
+    CircleCollider2D colliderLink;
+    PlayerInput inputLink;
+    [SerializeField] float speedX;
+    [SerializeField] float speedY; 
 
 
     void Start()
     {
         rigidLink = GetComponent<Rigidbody2D>();
+        colliderLink = GetComponent<CircleCollider2D>();
+        inputLink = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
@@ -28,10 +37,20 @@ public class PlayerControlls : MonoBehaviour
 
         velocityX = moveInputX * Time.deltaTime * speedModifier;
         velocityY = moveInputY * Time.deltaTime * speedModifier;
+        speedX = moveInputX;
+        speedY = moveInputY;
         rigidLink.velocity = new Vector2 (velocityX, velocityY);
+        transform.position = spawnLocation.transform.position;
 
 
 
     
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        moveInputX = moveInput.x;
+        moveInputY = moveInput.y;
     }
 }
