19f960500d0d1c2c8fb425d44e6f6e2b7ad131de
superinjustice1000 <110139914+superinjustice1000@users.noreply.github.com>
2024-10-18 15:05:13 -0400

    0.0.5 Player Basic controls
    The player character can now move, animate when in motion, and rotate to the direction it is moving in.

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.Windows;
 
 
 public class PlayerControlls : MonoBehaviour
@@ -20,7 +23,9 @@ public class PlayerControlls : MonoBehaviour
     //[SerializeField] float speedY; 
     [SerializeField] float averageSpeed;
     Animator animatorLink;
-    
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float rotationThreshold;
+
 
 
     void Start()
@@ -29,27 +34,29 @@ public class PlayerControlls : MonoBehaviour
         colliderLink = GetComponent<CircleCollider2D>();
         inputLink = GetComponent<PlayerInput>();
         transform.position = spawnLocation.transform.position;
-        animatorLink = GetComponent <Animator>();
+        animatorLink = GetComponent<Animator>();
         transform.Rotate(0, 0, 0);
-        
+
     }
 
     // Calls once every fixed number of Frames
     void FixedUpdate()
     {
         Move();
+        Rotate();
     }
 
-    private void Move() {
+    private void Move()
+    {
         velocityX = moveInputX * Time.deltaTime * speedModifier;
         velocityY = moveInputY * Time.deltaTime * speedModifier;
         //speedX = moveInputX;
         //speedY = moveInputY;
-        rigidLink.velocity = new Vector2 (velocityX, velocityY);
+        rigidLink.velocity = new Vector2(velocityX, velocityY);
         //transform.position = spawnLocation.transform.position;
         averageSpeed = Mathf.Abs((velocityX + velocityY) / 2);
         animatorLink.SetFloat("Speed", averageSpeed);
-        
+
     }
 
     public void OnMove(InputValue value)
@@ -57,5 +64,26 @@ public class PlayerControlls : MonoBehaviour
         moveInput = value.Get<Vector2>();
         moveInputX = moveInput.x;
         moveInputY = moveInput.y;
+    }
+
+    private void Rotate()
+    {
+        // Only rotate if there's movement
+        if (moveInputX != 0 || moveInputY != 0)
+        {
+            // Only rotate if the input is greater than a small threshold(to avoid jittering when input is close to zero)
+            if (Mathf.Abs(moveInputX) > rotationThreshold || Mathf.Abs(moveInputY) > rotationThreshold)
+            {
+                // Calculate the angle based on the input direction
+                float angle = Mathf.Atan2(moveInputY, moveInputX) * Mathf.Rad2Deg;
+
+                // Apply a rotation offset to correct the orientation (90 degrees or -90 degrees)
+                angle -= -90f;  // Adjust this value if necessary (e.g., -90 if the sprite is rotated another way)
+
+                // Smooth the rotation using rotationSpeed
+                Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
     }
 }
