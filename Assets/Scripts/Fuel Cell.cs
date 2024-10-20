a6019c82623a764edb2867074bb05f12ea4dac2b
ISOSPRITES <dunkyfan@gmail.com>
2024-10-19 21:22:46 -0400

    getters and setters to streamline fuel call logic

--- Assets/Scripts/Fuel Cell.cs
+++ Assets/Scripts/Fuel Cell.cs
@@ -6,13 +6,16 @@ public class FuelCell : MonoBehaviour
 {
     [SerializeField] GameObject playerLink;
     [SerializeField] PlayerControlls playerControlls;
+    [SerializeField] float iN;
+    [SerializeField] float oN;
+
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var hitTag = collision.tag;
         if(hitTag == "Player")
         {
-           
+           PlayerControlls.setRadius((getInnerRadius + iN),(getOuterRadius + oN))
         }
     }
 
